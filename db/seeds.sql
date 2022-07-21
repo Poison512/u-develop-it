38c39c603878b8f8707b50e5ed7d00bdbf24327e
Poison512 <adrian512@Adrians-MBP.localdomain>
2022-07-21 11:38:55 -0500

    created candidates table

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+('Ronald', 'Reagan', 1),
+('Bin', 'Laden', 1),
+('Spider', 'Man', 0),
+('Bruce', 'Wayne',1),
+('Donald', 'Trump', 0);
